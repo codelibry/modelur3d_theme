581dcee4107b76088d28c8489d5af0b002e7bd84
Aleksei <oleksiy.t@codelibry.dev>
2026-04-03 11:42:12 +0300

    FIx: Checkout qa

--- inc/woocommerce-hooks/checkout/login-message.php
+++ inc/woocommerce-hooks/checkout/login-message.php
@@ -3,7 +3,7 @@ add_action( 'woocommerce_before_checkout_form', 'custom_checkout_html_login_mess
 
 function custom_checkout_html_login_message() {
     if ( ! is_user_logged_in() && ! WC()->checkout()->is_registration_enabled() && WC()->checkout()->is_registration_required() ) {
-        echo '<div class="woocommerce-info">Please <a href="#popup-login-form" style="color: var(--color-primary)">log in here</a> to complete your purchase.</div>';
+        echo '<div class="woocommerce-info">Please <a href="#popup-login-form" style="color: var(--color-blakc)">log in here</a> to complete your purchase.</div>';
         
         remove_filter( 'woocommerce_checkout_must_be_logged_in_message', '__return_empty_string' ); 
         add_filter( 'woocommerce_checkout_must_be_logged_in_message', '__return_empty_string' );
